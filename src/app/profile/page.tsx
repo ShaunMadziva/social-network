40e8040f0146c0d8d738e251bc8222d86075ec2b
Shaun <shaunmadziva@gmail.com>
2024-10-21 11:58:50 +0100

    await profileResults

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -11,13 +11,13 @@ export default async function Profile() {
     `SELECT * FROM profiles WHERE clerk_user_id = $1`,
     [userId]
   );
-  const profile = profileResult.rows[0] || null;
+  const profile = await profileResult.rows[0] || null;
 
   const postsResult = await db.query(
     `SELECT * FROM posts WHERE clerk_user_id = $1`,
     [userId]
   );
-  const posts = postsResult.rows; // the list of posts {} so we can map over it
+  const posts = await postsResult.rows; // the list of posts {} so we can map over it
 
   return <ProfileClient profile={profile} posts={posts} />;
 }
